7471205c4bddb7b74646053bb9b13a5579d29026
Phuong Le <queenphuong@gmail.com>
2018-08-30 20:15:05 +0700

    Update to_upper_transformer.py

--- to_upper_transformer.py
+++ to_upper_transformer.py
@@ -1,9 +1,5 @@
-def transform(event):
-    try:
-        name_column_index = event['columns'].index('name')
-        for user in event['data']:
-            user[name_column_index] = user[name_column_index].upper()
-    except:
-        pass
-
-    return event
+def transform(entity):
+    if 'name' in entity:
+        entity['name'] = entity['name'].upper()
+        
+    return entity
